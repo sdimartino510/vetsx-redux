99fff04198046d9fd495a58dc5b9f687761bdcea
Sergio Di Martino <sergio@webdevserg.io>
2020-06-10 13:48:04 -0700

    Removes todo from user schema

--- models/User.js
+++ models/User.js
@@ -20,10 +20,6 @@ const UserSchema = new Schema({
     type: Date,
     default: Date.now(),
   },
-  todos: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Todo',
-  }],
 });
 // eslint-disable-next-line
 UserSchema.methods.toJSON = function () {
